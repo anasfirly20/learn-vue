39cb72726a1f3488372d60ad5935a73389d7fae2
Anas Firly <anasfirly20@gmail.com>
2023-08-30 08:28:15 +0400

    validating props

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3>From APP</h3>
+  <Greeting :age="age" />
+  <User :age="age" @age-change="updateAge"/>
+</template>
+
+<script>
+import Greeting from "@/components/Greeting.vue"
+import User from "@/components/User.vue"
+
+export  default {
+  name: 'App',
+  components: {
+    Greeting,
+    User
+  },
+  data(){
+    return {
+      age: 20
+    }
+  },
+  methods: {
+    updateAge(num){
+      this.age += num
+    }
+  }
+}
+</script>
+
+
---
+++ src/components/Greeting.vue
@@ -0,0 +1,24 @@
+<template>
+    <p v-if="age < 25">This message is only shown to ages above 25</p>
+    <p v-else>You are above 25 years old!</p>
+    
+   <p>{{msg}}</p>
+</template>
+
+<script>
+ export default {
+    name: 'Greeting',
+    props: ['age'],
+    data(){
+      return {
+        msg: 'Hello world'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+p:hover {
+    color: darken(#cc4444, 15%)
+}
+</style>
